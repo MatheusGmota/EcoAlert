55841a85f25edb1c3ff1f9b0e5daccba12c8a39c
MatheusGmota <livematgreg@gmail.com>
2025-06-08 15:00:07 -0300

    Adicionando configuracao para Swagger

--- EcoAlert/Program.cs
+++ EcoAlert/Program.cs
@@ -16,15 +16,24 @@ builder.Services.AddTransient<ILimiarRepository, LimiarRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
